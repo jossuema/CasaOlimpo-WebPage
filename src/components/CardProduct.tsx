774537c216253b114313898159bca00a8370a53f
jossuema <mmalla1@outlook.com>
2024-06-05 13:55:17 -0500

    Products Cards

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,32 +1,41 @@
 import React from "react";
+import { formatPrice } from "../utils";
 
 interface CardProductProps {
-  id: number;
-  title: string;
-  description: string;
-  marca: string;
-  price: number;
-  img_url: string;
+  producto: {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    precio: number,
+    marca: string;
+    img_url: string;
+  }
 }
 
-const CardProduct = () => {
+const CardProduct = ({producto}:CardProductProps) => {
+  const url = "../../public/images/products/"
+
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="h-80 overflow-hidden">
+    <div className="w-80 rounded overflow-hidden shadow-lg" key={producto.id}>
+      <div className="h-52 overflow-hidden">
         <img
-          className="w-full object-cover h-full"
-          src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+          className="h-52 object-cover"
+          src={url + producto.img_url}
           alt="card-image"
         />
       </div>
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Apple AirPods</div>
+        <div className="font-bold text-xl mb-2">{producto.nombre}</div>
         <p className="text-gray-700 text-base mb-4">
-          With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
+          {producto.descripcion}
         </p>
-        <div className="font-bold text-xl mb-2">$95.00</div>
+        <p className="text-gray-700 text-base mb-4">
+          {producto.marca}
+        </p>
+        <div className="font-bold text-xl mb-2">{formatPrice(producto.precio)}</div>
       </div>
-      <div className="px-6 pt-4 pb-2">
+      <div className="px-6 pt-1 pb-2">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Agregar al carrito
         </button>
